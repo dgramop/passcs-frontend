40e2e1ade1f7630facf8cd25d867f0a070f3fb58
Dhruv Gramopadhye <dgramop@gmail.com>
2022-02-15 16:23:32 -0500

    subscription cancellation, billing page

--- src/CustomerDashboard.jsx
+++ src/CustomerDashboard.jsx
@@ -16,16 +16,35 @@ async function skip_payment(payment) {
 		
 }
 
+async function cancel_subscription(subscription_id) {
+		try {
+				let req = await fetch("/subscriptions/"+subscription_id+"/cancel", { method: "POST" });
+				let res = await req.json();
+				if(res.error) throw res.error;
+				else return res;
+		} catch(e) {
+				if(e.type) throw e;
+				else throw {type: "Fetch Error"};
+		}
+		
+}
+
+function get_next_meeting(meeting_info) {
+		let next_meeting = meeting_info[0];
+		for(let meeting of meeting_info) {
+				if(meeting.meeting.occurrence_epoch < next_meeting.meeting.occurrence_epoch && next_meeting.meeting.payment_status !== "skipped") {
+						next_meeting = meeting;
+				}
+		};
+
+		return next_meeting;
+}
+
 /**
  * @param {props.title} Class name for the slotcard
  */
 function SlotCard(props) {
-		let next_meeting = props.meeting_info[0];
-		for(let meeting of props.meeting_info) {
-				if(meeting.meeting.occurrence_epoch < next_meeting.meeting.occurrence_epoch && next_meeting.meeting.payment_status !== "skipped") {
-						next_meeting = meeting;
-				}
-		}
+		let next_meeting = get_next_meeting(props.meeting_info);
 
 		let color_index = 0;
 		let course_integer = next_meeting.course.course_number.match(/[0-9]+/g)
@@ -34,7 +53,6 @@ function SlotCard(props) {
 		}
 
 		let [skipConfirmOpen, setSkipConfirmOpen] = useState(false);
-		let [skipLoading, setSkipLoading] = useState(false);
 
 
 		let {weekday, start_hour} = timezone_time_from_slot(props.slot);
@@ -71,28 +89,38 @@ function SlotCard(props) {
 						<Link to={"billing/"+next_meeting.meeting.slot}><Button secondary> Manage Billing </Button></Link>
 						{can_skip_next_meeting && <Button onClick={() => setSkipConfirmOpen(true)} > Skip Next Class </Button>}
 				</div>
-				{skipConfirmOpen && <Modal title="Skip Next Meeting" close={() => setSkipConfirmOpen(false)} buttons={
-						{
-								primary: {
-										text: "Skip Your "+(next_meeting_date.getMonth()+1)+"/"+next_meeting_date.getDate()+" Class",
-										onClick: async () => { setSkipLoading(true); await skip_payment(next_meeting.payment); await props.update(); setSkipConfirmOpen(false); setSkipLoading(false) /*TODO: just update this slot, needs backend endpoint*/ },
-										disabled: skipLoading
-								},
-								secondaries: [{
-										text: "Go Back",
-										onClick: () => setSkipConfirmOpen(false)
-								}]
-						}
-						}>
-						Skipping your next meeting may reduce the total number of classes you take by the end of the semester, and could impact your eligibility for the passCS Guarentee. In order to qualify, you must take at least 12 classes (<a href="/terms" target="_blank">see other terms</a>)
-				</Modal>}
+				{skipConfirmOpen && <SkipModal next_meeting_date={next_meeting_date} close={() => setSkipConfirmOpen(false)} next_meeting={next_meeting} update={props.update}/>}
 		</section>
 		);
 }
 
+function SkipModal(props) {
+		let [skipLoading, setSkipLoading] = useState(false);
+		return (<Modal title="Skip Next Meeting" close={() => props.close()} buttons={
+						{
+								primary: {
+										text: "Skip Your "+(props.next_meeting_date.getMonth()+1)+"/"+props.next_meeting_date.getDate()+" Class",
+										onClick: async () => { 
+												setSkipLoading(true);
+												await skip_payment(props.next_meeting.payment);
+												await props.update();
+												setSkipLoading(false);
+												props.close();
+										},
+										disabled: skipLoading
+								},
+								secondaries: [{
+										text: "Go Back",
+										onClick: () => props.close()
+								}]
+						}
+						}>
+						Skipping your next meeting may reduce the total number of classes you take by the end of the semester, and could impact your eligibility for the passCS Guarentee. In order to qualify, you must take at least 12 classes (<a href="/terms" target="_blank">see other terms</a>)
+				</Modal>)
+}
+
 
 export default function CustomerDashboard(props) {
-		let [skipConfirmOpen, setSkipConfirmOpen] = useState(false);
 
 		let [slots, setSlots] = useState({});
 		let [triedSlots, setTriedSlots] = useState(false); // so we can tell the difference between loading and empty states
@@ -129,12 +157,7 @@ export default function CustomerDashboard(props) {
 
 				<Routes>
 						<Route index element={<>
-										<header className="header">
-												<div className="header__content">
-														<img className="header__icon" src={"/flag192.png"} alt="passCS icon: a green pennant flag"/>
-														<h1 className="header__title"> Your Classes </h1>
-												</div>
-										</header>
+										<Header title="Your Classes"/>
 										<main className="customer_dashboard_main container">
 												{slots && Object.keys(slots).map((slot_id) => (
 														<SlotCard {...slots[slot_id]} update={get_slots} />
@@ -142,27 +165,64 @@ export default function CustomerDashboard(props) {
 												{error && <span className="form__error">{error}</span>}
 										</main>
 						</>}/>
-						<Route path="billing/:slot_id" element={<BillingInfo slots={slots} />}/>
+						<Route path="billing/:slot_id" element={<BillingInfo slots={slots} update={() => get_slots()}/>}/>
 				</Routes></>
 		)
 }
 
 function BillingInfo(props) {
+		let [skipConfirmOpen, setSkipConfirmOpen] = useState(false);
+		let [cancelModalOpen, setCancelModalOpen] = useState(false)
 		let params = useParams();
 		if(!params || !params.slot_id || props.slots == null || props.slots[params.slot_id]==null) {
 				return (
 						<Navigate replace to="/dashboard" />
 				);
 		};
+		let next_meeting = get_next_meeting(props.slots[params.slot_id].meeting_info)
+		let subscription_string = props.slots[params.slot_id].subscription?.payment_status.replace("_", " ") || "";
+		if(subscription_string==="trialing") subscription_string = "Active (First Meeting)";
+		
+		let can_skip_next_meeting = next_meeting.payment.payment_status === "subscription_pending" 
+
+		let cancelbuttonconfig = {
+				primary: {
+						text: "Go Back",
+						onClick: () => setCancelModalOpen(false)
+				},
+				secondaries: [
+						{
+								text: "End Upcoming Payments",
+								onClick: async () => {
+										await cancel_subscription(props.slots[params.slot_id].subscription.id);
+										await props.update()
+										setCancelModalOpen(false);
+								},
+								extraClasses: "button--secondary--red"
+						},
+						
+				]
+		}
+
+		if(can_skip_next_meeting) {
+				cancelbuttonconfig.secondaries.push({ 
+						text: "Skip Next Meeting",
+						onClick: () => {
+								setCancelModalOpen(false);
+								setSkipConfirmOpen(true);
+						}
+				})
+		}
+
 		return (
 				<>
-						<header className="header">
-								<div className="header__content">
-										<img className="header__icon" src={"/flag192.png"} alt="passCS icon: a green pennant flag"/>
-										<h1 className="header__title"> Billing Details </h1>
-								</div>
-						</header>
-						<main>
+						{cancelModalOpen && <Modal title="Confirm Cancellation" close={()=>setCancelModalOpen(false)} buttons={cancelbuttonconfig}>
+								Cancelling all of your scheduled meetings with passCS early may disqualify you from the passCS Guarentee (see <a href="terms">terms</a>).
+						</Modal> }
+						{skipConfirmOpen && <SkipModal next_meeting_date={new Date(next_meeting.meeting.occurrence_epoch * 1000)}  next_meeting={next_meeting} update={props.update} close={() => {setSkipConfirmOpen(false)}} />}
+						<Header title="Billing Details" />
+						<main className="container">
+								<Link to="/dashboard"><div className="paymenthistory__back"><span className="material-icons">west</span> <span>My Classes</span></div></Link>
 								<div className="paymenthistory">
 										<div className="paymenthistory__title">
 												Payment History
@@ -179,8 +239,8 @@ function BillingInfo(props) {
 																return (
 																<tr id={meeting_info.meeting.id}>
 																		<td className="paymenthistory__duedate">
-																				{due_date.getMonth()+1}/{due_date.getDate()}
-																		</td >
+																				{due_date.getMonth()+1}/{due_date.getDate()}/{due_date.getFullYear()}
+																		</td>
 																		<td className={"paymenthistory__paymentstatus "+((({"succeeded":"paymenthistory__paymentstatus--succeeded","subscription_pending":"paymenthistory__paymentstatus--subscription_pending", "processing":"paymenthistory__paymentstatus--processing"})[meeting_info.payment.payment_status] || "paymenthistory__paymentstatus--error"))}>
 																				{({"requires_confirmation":"Skipped","succeeded":"Succeeded","subscription_pending":"Scheduled", "processing":"Processing"})[meeting_info.payment.payment_status]}
 																		</td>
@@ -189,8 +249,37 @@ function BillingInfo(props) {
 														})}
 												</tbody>
 										</table>
+										<div className="paymenthistory__buttontray">
+												<div className={
+														"paymenthistory__paymentstatus "+((({"active":"paymenthistory__paymentstatus--succeeded",
+														"trialing":"paymenthistory__paymentstatus--subscription_pending",
+														"incomplete":"paymenthistory__paymentstatus--processing",
+														"incomplete-expired":"paymenthistory__paymentstatus--processing",
+														"unpaid":"paymenthistory__paymentstatus--error",
+														"canceled":"paymenthistory__paymentstatus--error",
+														"past_due":"paymenthistory__paymentstatus--error",
+														"incomplete_expired":"paymenthistory__paymentstatus--error"
+												})[props.slots[params.slot_id].subscription?.payment_status || "processing"]) || "paymenthistory__paymentstatus--error")}>
+														Autopay {subscription_string}
+												</div>
+
+												{props.slots[params.slot_id].subscription && <Button disabled={props.slots[params.slot_id].subscription.payment_status!=="active" } onClick={()=> setCancelModalOpen(true)}>
+														End Payments
+												</Button>}
+										</div>
 								</div>
 						</main>
 				</>
+		)
+}
+
+function Header(props) {
+		return (
+		<header className="header">
+				<div className="header__content">
+						<Link className="header__homelink" to="/"><img className="header__icon" src={"/flag192.png"} alt="passCS icon: a green pennant flag"/></Link>
+						<h1 className="header__title"> {props.title} </h1>
+				</div>
+		</header>
 		)
 }
