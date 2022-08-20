49dab5e11563771ab5479344d953bbaceb3b548d
Dhruv Gramopadhye <dgramop@gmail.com>
2022-08-20 14:11:22 -0400

    load courses from backend

--- src/PaymentFlow.jsx
+++ src/PaymentFlow.jsx
@@ -1,7 +1,7 @@
 import "./PaymentFlow.scss"
 import {ArrowBack} from '@mui/icons-material';
 import Select from 'react-select';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Button} from "./Components";
 
 export function RadioButton({children, name, value, onClick, selected, split, ...props}) {
@@ -29,8 +29,39 @@ export function RadioSelect({options, onChange, value, ...props}) {
 export default function PaymentFlow({embed, className, ...props}) {
 
 	// data loaded from the backend
-	const classOptions = useState(null)
-	const class_options = [{label:"Computer Science",options:[]}, {label:"Math",options:[]}, {label:"Other",options:[]}];
+	const [courseOptions, setCourseOptions] = useState([])
+	const [selectedCourse, setSelectedCourse] = useState(null);
+
+	useEffect(() => {
+		let get_classes = async () => {
+			let coursesresp = await fetch("/api/courses/")
+			let coursesdata = await coursesresp.json()
+
+			let compsci = []
+			let math = []
+			let other = []
+			for(let course of coursesdata.data) {
+				let subj = course.course_number.match(/[A-Za-z]+/g)[0]
+				let num = course.course_number.match(/[1-9]+/g)[0]
+
+				if(subj.toLowerCase() === "cs") {
+					compsci.push({label: `${course.course_name} (${course.course_number})`, value: course.id, num:num})
+				} else if(subj.toLowerCase() === "math") {
+					math.push({label: `${course.course_name} (${course.course_number})`, value: course.id, num:num})
+				} else {
+					other.push({label: `${course.course_name} (${course.course_number})`, value: course.id, num:num})
+				}
+			}
+
+			const course_options = [{label:"Computer Science",options:compsci}, {label:"Math",options:math}, {label:"Other",options:other}];
+			setCourseOptions(course_options)
+		}
+
+		get_classes()
+	}, [])
+
+	//TODO: load slots by class
+
 
 	// form control
 	const [size, setSize] = useState(null);
@@ -56,8 +87,8 @@ export default function PaymentFlow({embed, className, ...props}) {
 
 			<section className="payflow__inputs">
 				<section className="payflow__inputgroup">
-					<h3 className="payflow__inputgroup__title">What class can we help you with?</h3>
-					<Select placeholder="Select or type..." className="payflow__inputgroup__select" options={class_options} />
+					<h3 className="payflow__inputgroup__title">What course can we help you with?</h3>
+					<Select placeholder="Select or type..." className="payflow__inputgroup__select" options={courseOptions} />
 				</section>
 
 				<section className="payflow__inputgroup">
