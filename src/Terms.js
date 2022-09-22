7c6a907c9dd040a12d5d971c947bc52b5f6c1896
Dhruv Gramopadhye <dgramop@gmail.com>
2022-09-22 15:41:56 -0400

    terms updates

--- src/Terms.js
+++ src/Terms.js
@@ -13,13 +13,13 @@ export default function Terms(props) {
 						<li>You failed the class (F or D final grade), and</li>
 						<li>You completed the course (withdrawals, incomplete do not count), and</li>
 						<li>You did not have receive a grade high enough to allow the course to fill any of your degree requirements, and</li>
-						<li>You attended at least 12 classes of one-on-one tutoring in the class you failed for a minimum of 12 hours of total one-on-one passCS instruction for the class you failed. For passCS Guarantee computations only, one hour of one-on-two instruction shall be equivalent to 40 minutes of one-on-one instruction, and</li>
+						<li>You attended during the same semester you were enrolled, at least 12 classes of one-on-one tutoring in the class you failed for a minimum of 12 hours of total one-on-one passCS instruction for the class you failed. For passCS Guarantee computations only, one hour of one-on-two instruction shall be equivalent to 40 minutes of one-on-one instruction, and</li>
 						<li>You made a concerted effort to pass, </li>
 						<li>You made no effort to fail</li>
 						<li>You didn’t violate your university’s honor code or receive any sanction (including but not limited to course failure) for cheating (includes “plea deals” with professors that don’t receive an official honor council hearing), and</li>
 						<li>You attended all your exams and submit all assignments, for example, you may not avail of this guarantee if you overslept your final and failed</li>
 						<li>At the time you contract our services, your grades must be high enough that a reasonable increase in performance would be sufficient to pass the class</li>
-						<li>You must be enrolled in the class</li>
+						<li>You must be currently enrolled in the class</li>
 					</ul>
 
 					If you fail, you must provide evidence of your course failure, itemized individual assignment grades, test scores, and feedback from your professor, and evidence that you qualify for the passCS Guarantee in writing to dhruv@passcs.io within 7 days of grades being posted, or two weeks following the end of finals, whichever comes first.<br/><br/>
@@ -27,6 +27,7 @@ export default function Terms(props) {
 					<b>Parameters</b>
 					<ul>
 						<li>You will not be refunded for no-show meetings or skipped meetings</li>
+						<li>You will not be refunded for meetings that occurred before or after the semester you were enrolled in the class at your university</li>
 						<li>You will not be refunded more than what you paid passCS Inc. that semester, for the specific class you failed</li>
 						<li>You will not be refunded tuition you paid to your university or college</li>
 						<li>You agree not to hold passCS liable for any damages caused by failing or otherwise not passing a class</li>
@@ -47,6 +48,7 @@ export default function Terms(props) {
 					As an additional courtesy, you may receive a full refund for the first meeting that you cancel fewer than 25 hours in advance, so long as you notify your tutor at least 2 hours in advance of the meeting. To claim this courtesy, you must email dhruv@passcs.io within 7 days.<br/><br/>
 
 					The no-show and full-refund courtesies may automatically be applied, without your request; they will decrement the number of courtesies you have left. <br/><br/>
+					If you paid as part of a bulk purchase, instead of recieving a refund you will recieve credit towards your account for one meeting.
 				</p>
 			</section>
 
@@ -56,6 +58,8 @@ export default function Terms(props) {
 					If you sign up for a weekly or recurring time slot, you authorize passCS to charge your card through our payment processor, Stripe, to pay for all meetings you attend with our tutors, including meetings not scheduled through our system. The rates provided during checkout assume 1 hour of instruction per week. If you meet more than this, additional meetings may be added to what you already pay
 				</p>
 			</section>
-		</div><br/><small> Last updated March 31, 2022. Removed the midterm/semester midpoint requirement </small>
+		</div><br/>
+		<small> Last Updated September 22, 2022. Only allow refund of classes that were taken within the same semester</small><br/>
+		<small> March 31, 2022: Removed the midterm/semester midpoint requirement </small>
 	</>)
 }
