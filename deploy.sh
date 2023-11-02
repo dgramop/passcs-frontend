314380218c26f8485b4c15eb9d7aeb4c8af56c5d
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2023-11-02 14:47:53 -0400

    deply fixes

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-echo $SSH_PRIVATE_KEY > "/tmp/id_rsa"
-ssh -i /tmp/id_rsa "yes | cp -r frontend_build/* /usr/share/nginx/html/"
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> /tmp/id_rsa
+echo $SSH_PRIVATE_KEY >> /tmp/id_rsa
+echo "-----END OPENSSH PRIVATE KEY-----" >> /tmp/id_rsa
+chmod 600 /tmp/id_rsa
+
+apk update
+apk add openssh
+
+SSH_OPTS="-o StrictHostKeyChecking=no -i /tmp/id_rsa"
+
+ssh $SSH_OPTS "yes | cp -r frontend_build/* /usr/share/nginx/html/"
